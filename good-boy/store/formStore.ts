5e8ff8512e19cc95e5d9a3da04ffce082df5127a
Martin Fuzy <martin.fuzy@gate-wear.com>
2026-02-07 10:37:57 +0100

    add basic structure of store

---
+++ good-boy/store/formStore.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+
+type UserState = {
+  name: string;
+  surname: string;
+  email: string;
+  phoneNumber: string;
+
+  isChecked: boolean;
+
+  setName: (value: string) => void;
+  setSurname: (value: string) => void;
+  setEmail: (value: string) => void;
+  setPhoneNumber: (value: string) => void;
+
+  toggleChecked: () => void;
+};
+
+export const useUserStore = create<UserState>((set) => ({
+  name: "",
+  surname: "",
+  email: "",
+  phoneNumber: "",
+
+  isChecked: false,
+
+  setName: (value) => set({ name: value }),
+  setSurname: (value) => set({ surname: value }),
+  setEmail: (value) => set({ email: value }),
+  setPhoneNumber: (value) => set({ email: value }),
+
+  toggleChecked: () => set((state) => ({ isChecked: !state.isChecked })),
+}));
