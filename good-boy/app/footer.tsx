31abe09801bd2d6833ac6855d1b496ac9a9b1ab5
Martin Fuzy <martin.fuzy@gate-wear.com>
2026-02-08 20:39:19 +0100

    addd footer styles

--- good-boy/app/footer.tsx
+++ good-boy/app/footer.tsx
@@ -3,20 +3,16 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <div className="footer">
-      <Image
-        src="/Mascot.png"
-        alt="Pes"
-        width={40}
-        height={60}
-        style={{
-          height: "100%",
-          width: "auto",
-        }}
-        priority
-      />
-      <Image src="/GB.png" alt="Good Boy" width={40} height={10} />
-      <Image src="/Socials.png" alt="Socials" width={40} height={10} />
-      Kontakt O projekte
+      <div className="footerLeft">
+        <Image src="/Mascot.png" alt="Mascot" width={40} height={60} />
+        <Image src="/GB.png" alt="Good Boy" width={80} height={10} />
+      </div>
+
+      <div className="footerRight">
+        <Image src="/Socials.png" alt="Socials" width={60} height={10} />
+        <a href="/contact">Kontakt</a>
+        <a href="/about">O projekte</a>
+      </div>
     </div>
   );
 }
